027485deb56157b364ca3d3608dfbdf4ac762f89
yntc7 <lucaspereiraa267@gmail.com>
2026-04-02 08:51:54 -0300

    Arquivos em Funções Python
    Utilizando Python em ambiente Linux para criar diretório com permissão 744.

--- Ex21.py
+++ Ex21.py
@@ -1,42 +1,89 @@
-n1 = 0
-n2 = 0
-n3 = 0
-n4 = 0
-media = 0
-tipo = 0
+import os
+
+#Variáveis globais
+nome: str = ''
+nota1: float = 0
+nota2: float = 0
+nota3: float = 0
+nota4: float = 0
+valor_media: float = 0
+dir: str = ''
+arq: str = ''
+
+def criar_diretorio():
+    global dir
+
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+
+    os.chmod(dir, 0o744)
+
+def med(n1, n2, n3, n4):
+    media = (n1 + n2 + n3 + n4) / 4
+    return media
+
+
+def escreveArq(caminho, arquivo, linha_arq):
+    file = ""
+    tipo = ""
+    enc = ""
+
+    if os.path.exists(caminho) and os.path.isdir(caminho):
+
+        caminho_arquivo = os.path.join(caminho, arquivo)
+
+        if os.path.exists(caminho_arquivo):
+            tipo = 'a'
+        else:
+            tipo = 'w'
+
+        with open(caminho_arquivo, tipo) as file:
+            file.write(linha_arq)
+
+
+def cadastro(nm, nt1, nt2, nt3, nt4, vlr_med):
+    global dir, arq
+
+    linha = (
+        nm + ":" +
+        str(nt1) + ";" +
+        str(nt2) + ";" +
+        str(nt3) + ";" +
+        str(nt4) + ";" +
+        str(vlr_med) + "\n"
+    )
+
+    escreveArq(dir, arq, linha)
+
 
 def entrada():
-    global n1, n2, n3, n4
-    n1= float(input("Digite a nota do primeito bimestre do aluno: ")) 
-    n2= float(input("Digite a nota do segundo bimestre do aluno: ")) 
-    n3 = float(input("Digite a nota do terceiro bimestre do aluno: ")) 
-    n4 = float(input("Digite a nota do quarto bimestre do aluno: "))
+    global nome, nota1, nota2, nota3, nota4, valor_media
 
-def processamento():
-    global n1, n2, n3, n4, media, tipo
-    media = ((n1+n2+n3+n4)/4)
+    nome = input("Digite o nome do aluno: ")
+    nota1 = float(input("Nota 1: "))
+    nota2 = float(input("Nota 2: "))
+    nota3 = float(input("Nota 3: "))
+    nota4 = float(input("Nota 4: "))
 
-    if media >= 6:
-        tipo = 1 #Aprovado
-    elif media >= 3:
-        tipo = 2 #Exame
-    else: 
-        tipo = 3 #Retido
+    valor_media = med(nota1, nota2, nota3, nota4)
 
-def saida():
-    global media, tipo
-    print("A média aritmética das notas bimestrais do aluno é igual a: ", media)
+    print(f"Média de {nome}: {valor_media}")
 
-    if tipo == 1:
-        print("APROVADO")
-    elif tipo == 2:
-        print('EXAME')
-    else:
-        print('RETIDO')
-    
+    cadastro(nome, nota1, nota2, nota3, nota4, valor_media)
+
+
 def main():
-    entrada()
-    processamento()
-    saida()
+    global dir, arq
+
+    contador = 1
+
+    dir = '/tmp/exercicios'
+    arq = 'Ex21.txt'
+
+    criar_diretorio()
+
+    while contador <= 5:
+        entrada()
+        contador += 1
 
 main()
--- Ex34.py
+++ Ex34.py
@@ -1,8 +1,62 @@
-num = int(input('Digite um número: '))
+import os
 
-tabuada = 1
+#Variáveis globais
+valor: int = 0
+dir: str = ''
+arq: str = ''
 
-while tabuada <= 10:
-    rtabuada = (num*tabuada)
-    print(tabuada, "x", num, "=" ,rtabuada)
-    tabuada += 1
+def criar_diretorio():
+    global dir
+
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+
+    os.chmod(dir, 0o744)
+
+def mult(vlr, tab):
+    res = vlr * tab
+    return res
+
+def grava(c, rslt):
+    global dir, arq
+
+    file = ""
+    tipo = ""
+    enc = ""
+    linha = str(rslt) + '\n'
+
+    if os.path.exists(dir) and os.path.isdir(dir):
+
+        caminho = os.path.join(dir, arq)
+
+        # Define tipo de escrita
+        if os.path.exists(caminho) and c > 0:
+            tipo = 'a'
+        else:
+            tipo = 'w'
+
+        with open(caminho, tipo) as file:
+            file.write(linha)
+
+def main():
+    global valor, dir, arq
+
+    contador = 1
+    result = 0
+
+    dir = '/tmp/exercicios'
+    arq = 'Ex34.txt'
+
+    criar_diretorio()
+
+    valor = int(input("Digite um valor entre 1 e 10: "))
+
+    while contador <= 10:
+        result = mult(valor, contador)
+        print(f"{contador} x {valor} = {result}")
+
+        grava(contador, result)
+
+        contador += 1
+
+main()
